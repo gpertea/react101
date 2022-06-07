60974f75195e94de9490a00f59b24b76377d9078
Geo Pertea <geo.pertea@gmail.com>
2022-06-07 19:00:14 -0400

    added matching tabIDs to the database

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css'
 import './styles.css'
 import { useState, useEffect } from 'react'
 
-const tabs=["RNAseq", "DNAmet", "WGS"]
+const tabs  = ["RNAseq", "DNAmet", "WGS"] // tab display labels
+const tabIds= ["rnaseq", "dnam",   "wgs"] // corresponding data type IDs in database
 
 function App() {
   let date = new Date().toLocaleString()
-  const [activeTab, setActiveTab] = useState(tabs[0])
+  const [tab, setTab] = useState(0) //numeric ID of the selected tab
 
-  //mount/dismount hook
+  // mount/dismount hook
   useEffect(() => {
     console.log(" >> App mounted in DOM");
     return ()=> {
@@ -16,12 +17,12 @@ function App() {
     }
   }, []) // <-- empty dependency array means mount/dismount hook
 
-  //use
+  // useEffect post-render hook
   useEffect(() => {
-    console.log(` --- useEffect: post-render as data type changed to ${activeTab}`)
-  }, [activeTab]) //<-- dependency array
+    console.log(`  -- useEffect: post-render after tab changed to ${tabs[tab]}`)
+  }, [tab]) //<-- dependency array
 
-  console.log(`~~~~ App rendering with data type: ${activeTab}`)
+  console.log(`~~~~ App rendering with tab: ${tabs[tab]}`)
 
   return (
     <div className="App">
@@ -29,13 +30,13 @@ function App() {
         <h4>{date}</h4>
         <h3>Datasets</h3>
         <div className="tabs">
-          { tabs.map( (item, idx) =>
-            <span className={activeTab===item ? "tab-hdr sel" : "tab-hdr"} >
-            <button onClick={()=>setActiveTab(item)}>{item}</button>
+          { tabIds.map( (dbId, idx) =>
+            <span key={dbId} className={tab===idx ? "tab-hdr sel" : "tab-hdr"} >
+              <button onClick={()=>setTab(idx)}>{tabs[idx]}</button>
             </span>
             )}
          <div className="tab-content">
-           <div style={{padding: "1em 0"}}>Data type: {activeTab} </div>
+           <div style={{padding: "1em 0"}}>Data type: {tabs[tab]} </div>
          </div>
        </div>
         <br/>
