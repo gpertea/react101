037dd670500d02d19fc8d13d94d96f0e4e132931
Geo Pertea <geo.pertea@gmail.com>
2022-06-07 21:02:57 -0400

    fetch functionality added

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import { useState, useEffect } from 'react'
 const tabs  = ["RNAseq", "DNAmet", "WGS"] // tab display labels
 const tabIds= ["rnaseq", "dnam",   "wgs"] // corresponding data type IDs in database
 
+const mwSrv="http://gdebsrv:4095/pgdb/dslist"; //  + /tabId[tab]
+
 function App() {
   let date = new Date().toLocaleString()
   const [tab, setTab] = useState(0) //numeric ID of the selected tab
+  const [datasets, setDatasets]=useState([]) //data fetched asynchronously
 
   // mount/dismount hook
   useEffect(() => {
@@ -19,11 +22,17 @@ function App() {
 
   // useEffect post-render hook
   useEffect(() => {
-    console.log(`  -- useEffect: post-render after tab changed to ${tabs[tab]}`)
+    //console.log(`  -- useEffect: post-render after tab changed to ${tabs[tab]}`)
+    fetch(`${mwSrv}/${tabIds[tab]}`)
+     .then(res => res.json())
+     .then(json => { //async code
+         console.log("### received and decoded: ", json)
+         setDatasets(json)
+        })
   }, [tab]) //<-- dependency array
 
   console.log(`~~~~ App rendering with tab: ${tabs[tab]}`)
-
+  const dataReady=(datasets && datasets.length)
   return (
     <div className="App">
       <div className="content">
@@ -36,7 +45,25 @@ function App() {
             </span>
             )}
          <div className="tab-content">
-           <div style={{padding: "1em 0"}}>Data type: {tabs[tab]} </div>
+           { dataReady ?
+           <div>
+           <table className="tbl">
+             <thead>
+               <tr>
+                 { datasets[0].map((h)=><th>{h}</th>) }
+               </tr>
+             </thead>
+             <tbody>
+             { datasets.slice(1).map( (r,i) =>
+               <tr>{ r.map( (c,i) =>
+                  <td>{c}</td>) }
+               </tr>)
+             }
+             </tbody>
+           </table>
+           </div>
+           : <h3> Loading </h3>
+           }
          </div>
        </div>
         <br/>
